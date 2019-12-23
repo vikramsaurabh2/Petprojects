594ffb2fcdd93bc1834b7436f10aa16e52e7b2c5
vikramsaurabh2 <59121805+vikramsaurabh2@users.noreply.github.com>
2019-12-22 16:50:44 -0800

    Add files via upload
    added bubble sort as well

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ def main():
  print ("This is main function")
  global_arr = sortarr()
  #global_arr.arr = [21,23,34,45,56,57,59,60,0]
- #global_arr.insert()
+ global_arr.insert()
+ #global_arr.showarr(global_arr.arr)
  #final_array=global_arr.sort(global_arr.arr)
- final_array=global_arr.sort_sorted(global_arr.arr)
+ #final_array=global_arr.sort_sorted(global_arr.arr)
+ final_array=global_arr.bubble_sort(global_arr.arr)
  global_arr.showarr(final_array)
 
 if __name__=='__main__':
